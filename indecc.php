9c5dfc1f68efce552b9e6ca44ad85a7409065b93
intan290302 <khoirunnisaintan3@gmail.com>
2024-11-08 13:47:26 +0700

    komit pertama

---
+++ indecc.php
@@ -0,0 +1,39 @@
+<?php
+const nama ="intan";
+$umur = "22";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data pribadi</title>
+</head>
+<body>
+<table border = "1"  cellspacing ="0" cellpending ="7px">
+			<tr>
+				<td> Nama </td>
+				<td> : </td>
+				<td> <?php echo nama ?></td>
+			</tr>
+			<tr>
+				<td> umur</td>
+				<td> : </td>
+				<td> <?php echo 22 ?></td>
+			</tr>
+
+			<tr>
+				<td> tinggi</td>
+				<td> : </td>
+				<td> 150cm</td>
+			</tr>
+			<tr>
+				<td> berat badan</td>
+				<td> : </td>
+				<td> 42 kg</td>
+			</tr>
+		</table>
+</body>
+</html>
